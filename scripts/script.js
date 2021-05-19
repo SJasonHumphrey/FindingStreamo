949517d125d0ba8af8adfd8759b9cf8d9d6413ce
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-05-19 16:27:43 -0500

    final layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-
-
 // GLOBAL VARIABLES
 
 
@@ -197,9 +195,6 @@ function fadeOut(id) {
 
 
 
-
-
-
 // SELECTING DIV TO SHOW BY PARENT NODES CHILDREN SELECTOR
 function onMediaHover() {
     const mediaItem = document.querySelectorAll('.media-item');
@@ -208,14 +203,6 @@ function onMediaHover() {
         item.onmouseleave = () => item.children[2].style.display = 'none';
     });
 };
-
-
-
-
-
-
-
-
 
 
     // MAIN NAVIGATION
@@ -247,7 +234,7 @@ function nav(param) {
             break;
     };
 
-    // RESET STYLES NEED 
+    // RESET STYLES NEED - IM NOT USING REACT HERE!!
     resetMediaResults();
     resetPagination();
     resetFullMediaContent();
@@ -489,8 +476,6 @@ function updateList(tmdbId, id) {
 };
 
 
-
-
     // ADD NEW LIST
 
 
@@ -513,8 +498,6 @@ function addNewList(divId, inputId) {
 };
 
 
-
-
     // ADD ITEM TO LIST 
 
 
@@ -529,10 +512,7 @@ function addItemToList(list, tmdbId, id) {
 }
 
 
-
-
     // DELETE A LIST
-
 
 function deleteList(list, id) {
 
@@ -560,13 +540,9 @@ function deleteList(list, id) {
 };
 
 
-
-
     // DELETE LIST ITEM
 
-
 function deleteItemFromList(list, tmdbId, id, remove) {
-
 
     for (let i in state.mylists[list]) {
 
@@ -593,13 +569,7 @@ function deleteItemFromList(list, tmdbId, id, remove) {
     };
 };
 
-
-
-
-
-
     // SHOW MAIN CONTENT RESULTS
-
 
 function showContentResults(results) {
 
@@ -731,7 +701,6 @@ function showFullMediaContent(result) {
         <p class="content-title">MEDIA DETAILS
             <i class="material-icons close-media-content" onclick="resetFullMediaContent(); checkIfCollectionChanged(${tmdbId})">close</i>
         </p>
-
         <!-- MEDIA BACKDROP -->
         <div id="media-showcase" style="background-image: url('${backdrop}')">
             <a class="download-fanart" href="${backdrop}"target="_blank">DOWNLOAD WALLPAPER<br/>
@@ -742,7 +711,6 @@ function showFullMediaContent(result) {
             <img width="8%" id="streamer" src="${streamLink}" alt="${linktext}">
             </a>
         </div>
-
         <!-- MEDIA DETAILS -->
         <div id="media-details">
             <img width="140" id="media-poster" src="${poster}" alt="${title}">
@@ -750,7 +718,6 @@ function showFullMediaContent(result) {
                 <a href="${trailer[0]}" target=_blank">Trailer</a>
                 <span>${rating}</span><span>${status}</span><span>${date}</span><span>${runTime}</span>
                 <span class="from-collection" onclick="updateList(${tmdbId},'#from-full-media-collection')">Add/Remove from Collection</span>
-
                 <!-- ADD REMOVE ITEM FROM COLLECTION -->
                 <div id="from-full-media-collection"></div>
             </div>
@@ -883,9 +850,11 @@ function showMyLists() {
 
 
 
-
-    // SAMPLE LIST DATA
-
+/*
+==============================
+    SAMPLE LIST DATA
+==============================
+*/
 
 function showNoListsText() {
     userLists.innerHTML = `
@@ -1551,3 +1520,11 @@ function init() {
     nav('movies,popular');
 };
 init();
+
+
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
